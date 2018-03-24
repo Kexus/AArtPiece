b0f375659e63226623192416c4ac52c764a44365
Kexus <anemeroff2014@my.fit.edu>
2018-03-23 23:26:22 -0400

    11pm snapshot

---
+++ Assets/Scripts/MenuScropt.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScropt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Play () {
+		//StartCoroutine(LoadGameScene());
+		Debug.Log("This would load the game scene if it existed");
+	}
+
+	public void Quit () {
+		Application.Quit ();
+	}
+
+	IEnumerator LoadGameScene()
+	{
+		// The Application loads the Scene in the background at the same time as the current Scene.
+		//This is particularly good for creating loading screens. You could also load the Scene by build //number.
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Game");
+
+		//Wait until the last operation fully loads to return anything
+		while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+	private Rigidbody2D body;
+	public float speedMultiplyer;
+	public float horizontalMultplyer;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent <Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (horizontalMultplyer*moveHorizontal, moveVertical);
+		body.AddForce (speedMultiplyer*movement);
+		if (Input.GetButtonDown ("Fire1")) {
+
+		}
+	}
+}
